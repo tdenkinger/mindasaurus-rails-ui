4ee4d60078cbcf8d228a8693ff70599553aeb434
Troy Denkinger <tdenkinger@gmail.com>
2013-11-06 11:29:23 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MindasaurusRailsGui::Application.config.secret_key_base = '654561d3e384ec0e5e32193d6058372fa23cf8ff5a0c8ad6a5830c922f84851d1730b59901a0271d51672ceea21b5756f2ad166f4a624fac5f2300c2cc60d711'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MindasaurusRailsGui::Application.config.session_store :cookie_store, key: '_mindasaurus-rails-gui_session'
